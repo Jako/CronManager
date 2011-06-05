aa172f7ba0b1c269ffb85cdce070e4f59084ee88
Bert Oost <bert@server.oostdesign.nl>
2011-06-05 19:58:45 +0200

    Initial version of the Cron Manager

--- _build/build.schema.php
+++ _build/build.schema.php
@@ -1,5 +1,39 @@
 <?php
 
+define('PKG_NAME', 'CronManager');
+define('PKG_NAME_LOWER', 'cronmanager');
 
+require_once dirname(__FILE__).'/build.config.php';
+include_once MODX_CORE_PATH . 'model/modx/modx.class.php';
+
+$modx = new modX();
+$modx->initialize('mgr');
+$modx->loadClass('transport.modPackageBuilder','',false, true);
+$modx->setLogLevel(modX::LOG_LEVEL_INFO);
+$modx->setLogTarget('HTML');
+ 
+$root = dirname(dirname(__FILE__)).'/';
+$sources = array(
+    'model' => $root.'core/components/'.PKG_NAME_LOWER.'/model/',
+    'schema_file' => $root.'core/components/'.PKG_NAME_LOWER.'/model/schema/'.PKG_NAME_LOWER.'.mysql.schema.xml',
+);
+$manager = $modx->getManager();
+$generator = $manager->getGenerator();
+ 
+if(!is_dir($sources['model'])) {
+	$modx->log(modX::LOG_LEVEL_ERROR,'Model directory not found!');
+	die();
+}
+
+if(!file_exists($sources['schema_file'])) {
+	$modx->log(modX::LOG_LEVEL_ERROR,'Schema file not found!');
+	die();
+}
+
+$generator->parseSchema($sources['schema_file'], $sources['model']);
+
+$modx->addPackage(PKG_NAME_LOWER, $sources['model']);
+$manager->createObjectContainer('modCronjob');
+$manager->createObjectContainer('modCronjobLog');
 
 ?>
--- _build/setup.options.php
+++ _build/setup.options.php
@@ -1,5 +1,15 @@
 <?php
 
-
+$output = '';
+switch ($options[xPDOTransport::PACKAGE_ACTION]) {
+    case xPDOTransport::ACTION_INSTALL:
+        $output = '<h2>Cron Manager Installer</h2>
+<p>Thanks for installing the Cron Manager! See the official ADDON documentation for more installation and configuration details.</p><br />';
+        break;
+    case xPDOTransport::ACTION_UPGRADE:
+    case xPDOTransport::ACTION_UNINSTALL:
+        break;
+}
+return $output;
 
 ?>
