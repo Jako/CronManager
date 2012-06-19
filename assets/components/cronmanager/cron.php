c7bbb2d861067ac98f5b6042965ab2c7a6cf188e
Kireev Vitaly <argnist88@gmail.com>
2012-06-19 14:44:16 +0800

    for access without authorization

--- assets/components/cronmanager/cron.php
+++ assets/components/cronmanager/cron.php
@@ -2,6 +2,10 @@
 /**
  * @var modX $modx
  */
+
+// for access without authorization
+define('MODX_REQP',false);
+
 require_once dirname(dirname(dirname(dirname(__FILE__)))).'/config.core.php';
 require_once MODX_CORE_PATH.'config/'.MODX_CONFIG_KEY.'.inc.php';
 require_once MODX_CONNECTORS_PATH.'index.php';
