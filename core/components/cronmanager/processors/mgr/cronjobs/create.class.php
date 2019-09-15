37e6769b75189daaa4f68f136454685d44df433c
Thomas Jakobi <thomas.jakobi@partout.info>
2019-09-15 11:31:42 +0200

    Cronjob could be activated on create

--- core/components/cronmanager/processors/mgr/cronjobs/create.class.php
+++ core/components/cronmanager/processors/mgr/cronjobs/create.class.php
@@ -15,7 +15,6 @@ class CronManagerCronjobCreateProcessor extends modObjectCreateProcessor
     public function beforeSave()
     {
         $this->object->set('minutes', $this->getProperty('minutes', 1));
-        $this->object->set('active', $this->getProperty('active', false));
 
         return parent::beforeSave();
     }
