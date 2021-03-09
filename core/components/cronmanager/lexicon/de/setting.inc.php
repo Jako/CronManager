3ea7a12dcb62976d155a78ff16fd59a6759ebf40
Weblate (bot) <hosted@weblate.org>
2021-03-09 21:32:08 +0100

    Translations update from Weblate (#34)
    * Added translation using Weblate (French)
    
    * Translated using Weblate (German)
    
    Currently translated at 100.0% (44 of 44 strings)
    
    Translation: MODX CronManager/Standard
    Translate-URL: https://hosted.weblate.org/projects/modx-cronmanager/standard-lexicon/de/
    
    * Translated using Weblate (French)
    
    Currently translated at 100.0% (44 of 44 strings)
    
    Translation: MODX CronManager/Standard
    Translate-URL: https://hosted.weblate.org/projects/modx-cronmanager/standard-lexicon/fr/
    
    * Translated using Weblate (German)
    
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: MODX CronManager/System Settings
    Translate-URL: https://hosted.weblate.org/projects/modx-cronmanager/system-settings/de/
    
    * Translated using Weblate (French)
    
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: MODX CronManager/System Settings
    Translate-URL: https://hosted.weblate.org/projects/modx-cronmanager/system-settings/fr/
    
    Co-authored-by: Thomas Jakobi <buero@treehillstudio.de>

--- core/components/cronmanager/lexicon/de/setting.inc.php
+++ core/components/cronmanager/lexicon/de/setting.inc.php
@@ -5,8 +5,7 @@
  * @package cronmanager
  * @subpackage lexicon
  */
-
-$_lang['setting_cronmanager.cronjob_id'] = 'Cronjob ID';
-$_lang['setting_cronmanager.cronjob_id_desc'] = 'String, der als cronjob_id-Parameter zur cronjob-URL hinzugefügt werden muss.';
+$_lang['setting_cronmanager.cronjob_id'] = 'Cronjob-ID';
+$_lang['setting_cronmanager.cronjob_id_desc'] = 'String, der als cronjob_id-Parameter zur Cronjob-URL hinzugefügt werden muss.';
 $_lang['setting_cronmanager.debug'] = 'Debug';
 $_lang['setting_cronmanager.debug_desc'] = 'Debug-Informationen im MODX Fehlerprotokoll ausgeben.';
