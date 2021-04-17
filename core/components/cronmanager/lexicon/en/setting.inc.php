6da3b2aec5262577237aeb361cbf47729bcfca49
Allan Nordhøy <epost@anotheragency.no>
2021-04-17 05:07:07 +0000

    Translated using Weblate (English)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: MODX CronManager/System Settings
    Translate-URL: https://hosted.weblate.org/projects/modx-cronmanager/system-settings/en/

--- core/components/cronmanager/lexicon/en/setting.inc.php
+++ core/components/cronmanager/lexicon/en/setting.inc.php
@@ -5,8 +5,7 @@
  * @package cronmanager
  * @subpackage lexicon
  */
-
 $_lang['setting_cronmanager.cronjob_id'] = 'Cronjob ID';
-$_lang['setting_cronmanager.cronjob_id_desc'] = 'String, that has to be added to the cronjob url as cronjob_id parameter.';
+$_lang['setting_cronmanager.cronjob_id_desc'] = 'String, that has to be added to the cronjob URL as cronjob_id parameter.';
 $_lang['setting_cronmanager.debug'] = 'Debug';
 $_lang['setting_cronmanager.debug_desc'] = 'Log debug informations in MODX error log.';
