07229d81e29795dd773a073f296de38c3cd55103
Thomas Jakobi <thomas.jakobi@partout.info>
2022-09-10 09:14:40 +0200

    Improved fix for showing NaN dates in Safari

--- source/js/mgr/helper/util.js
+++ source/js/mgr/helper/util.js
@@ -2,6 +2,7 @@ CronManager.util.dateRenderer = function (format) {
     return function (v) {
         if (typeof v === 'string' && v !== '' && Date.parseDate(v, 'Y-m-d H:i:s')) {
             var format = (format) ? format : MODx.config.manager_date_format + ' ' + MODx.config.manager_time_format;
+            v = Date.parseDate(v, 'Y-m-d H:i:s');
             return Ext.util.Format.date(v, format);
         } else {
             return v;
