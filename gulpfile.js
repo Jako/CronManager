f84e2204a53c2deebabbcf7e4673cd0dc315f719
Thomas Jakobi <thomas.jakobi@partout.info>
2024-04-01 00:19:47 +0200

    Update to Gulp 5

--- gulpfile.js
+++ gulpfile.js
@@ -80,7 +80,7 @@ const sassMgr = function () {
 gulp.task('sass', gulp.series(sassMgr));
 
 const imagesMgr = function () {
-    return gulp.src('./source/img/**/*.+(png|jpg|gif|svg)')
+    return gulp.src('./source/img/**/*.+(png|jpg|gif|svg)', {encoding: false})
         .pipe(gulp.dest('assets/components/cronmanager/img/'));
 };
 gulp.task('images', gulp.series(imagesMgr));
