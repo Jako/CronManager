231161574c533f426379c8b973c715998486095c
Thomas Jakobi <thomas.jakobi@partout.info>
2024-08-29 12:33:34 +0200

    Normalize code

--- assets/components/cronmanager/connector.php
+++ assets/components/cronmanager/connector.php
@@ -13,6 +13,7 @@ require_once MODX_CORE_PATH . 'config/' . MODX_CONFIG_KEY . '.inc.php';
 require_once MODX_CONNECTORS_PATH . 'index.php';
 
 $corePath = $modx->getOption('cronmanager.core_path', null, $modx->getOption('core_path') . 'components/cronmanager/');
+/** @var CronManager $cronmanager */
 $cronmanager = $modx->getService('cronmanager', 'CronManager', $corePath . 'model/cronmanager/', [
     'core_path' => $corePath
 ]);
