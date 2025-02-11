ac71f6d2ed9cdebab1f712ff9f6ed7ba33de149e
Thomas Jakobi <thomas.jakobi@partout.info>
2025-02-11 18:37:01 +0100

    - Check the namespaced settings permission for the listing of the system settings too

--- core/components/cronmanager/processors/mgr/settings/getlist.class.php
+++ core/components/cronmanager/processors/mgr/settings/getlist.class.php
@@ -13,11 +13,23 @@ if (file_exists(MODX_PROCESSORS_PATH . 'system/settings/getlist.class.php')) {
     class_alias(\MODX\Revolution\Processors\System\Settings\GetList::class, \modSystemSettingsGetListProcessor::class);
 }
 
+/**
+ * Class CronManagerSystemSettingsGetlistProcessor
+ */
 class CronManagerSystemSettingsGetlistProcessor extends modSystemSettingsGetListProcessor
 {
     public $languageTopics = ['setting', 'namespace', 'cronmanager:setting'];
     public $objectType = 'setting';
     public $primaryKeyField = 'key';
+
+    /**
+     * {@inheritDoc}
+     * @return bool
+     */
+    public function checkPermissions()
+    {
+        return !empty($this->permission) ? $this->modx->hasPermission($this->permission) || $this->modx->hasPermission('cronmanager_' . $this->permission) : true;
+    }
 
     /**
      * {@inheritDoc}
