17c48438685dbc12c225eeb81e5433f6be76c3ee
Thomas Jakobi <buero@treehillstudio.de>
2025-11-17 12:43:11 +0100

    Added translation using Weblate (Spanish)

---
+++ core/components/cronmanager/lexicon/es/setting.inc.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Setting Lexicon Entries for CronManager
+ *
+ * @package cronmanager
+ * @subpackage lexicon
+ */
+$_lang['setting_cronmanager.cronjob_id'] = '';
+$_lang['setting_cronmanager.cronjob_id_desc'] = '';
+$_lang['setting_cronmanager.debug'] = '';
+$_lang['setting_cronmanager.debug_desc'] = '';
+$_lang['setting_cronmanager.pass_modcronjob'] = '';
+$_lang['setting_cronmanager.pass_modcronjob_desc'] = '';
+$_lang['setting_cronmanager.purge_running'] = '';
+$_lang['setting_cronmanager.purge_running_desc'] = '';
